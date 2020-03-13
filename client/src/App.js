9370c60ec69e448290e6bed3600f5e28d4114a19
Gabriel Rodrigues <gabrieloliveira12488@gmail.com>
2020-03-13 12:53:20 -0300

    Adding SearchBar component into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,17 @@ import M from 'materialize-css/dist/js/materialize.min.js';
 import "./App.css";
 
 //Layout components
+import SearchBar from "./components/layouts/SearchBar";
 
 function App() {
   useEffect(()=>{
     M.AutoInit();
   })
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <SearchBar />
+    </div>
+  );
 }
 
 export default App;
