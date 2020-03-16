029855e3833baa82b6507e6f87fd554741d863d0
Gabriel Rodrigues <gabrieloliveira12488@gmail.com>
2020-03-16 20:58:18 -0300

    adding actions to reducer and the log

--- client/src/actions/logActions.js
+++ client/src/actions/logActions.js
@@ -1,11 +1,24 @@
 import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
 
-export const getLogs = () => {
-  return async(dispatch) =>{
-    setLoading();
+export const getLogs = () =>  async dispatch => {
+    try{
+      setLoading();
+      const res = await fetch("/logs");
+      const data = await res.json();
+  
+      dispatch({
+        type:GET_LOGS,
+        payload:data
+      })
+    }catch(e){
+      dispatch({
+        type:LOGS_ERROR,
+        payload:e.reponse.data
+      })
+    }
+  
+  };
 
-  }
-};
 
 export const setLoading = () => {
   return {
